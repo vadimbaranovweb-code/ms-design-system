de468a05c596c1c8275f54d60d7d94705d74a9ce
Vadim Baranov <vadimbaranovweb@gmail.com>
2026-03-23 11:16:27 +0300

    Initial Commit

---
+++ MSDesignSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MSDesignSystem
+//
+//  Created by Vadim Baranov on 23.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MSDesignSystem/MSDesignSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSDesignSystemApp.swift
+//  MSDesignSystem
+//
+//  Created by Vadim Baranov on 23.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MSDesignSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
